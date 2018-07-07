b10cddf014bbc1eacaa872a330b1ac7cf3ba6e36
C13Money <32652102+C13Money@users.noreply.github.com>
2018-07-07 00:46:05 -0500

    Create main.cpp
    Every journey begins with a single step

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*********************************************************
+
+	Program: Lab One
+	Author : Charles Eaton
+	Class  : Hello World
+	Date   : Beginning of Time
+
+**********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	cout << "Hello World"  << endl;
+}
